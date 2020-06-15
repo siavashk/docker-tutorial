eaeb03a9c02ca27638d1c826015baeb8f3f737b4
Siavash Khallaghi <siavash.khallaghi@clarius.me>
2020-06-14 21:53:22 -0700

    Added core functionality.

--- app/app.py
+++ app/app.py
@@ -1,33 +1,84 @@
-from typing import List, Dict
-from flask import Flask
-import mysql.connector
+import cv2
+from datetime import datetime
+from flask import Flask, request, Response
 import json
+import mysql.connector
+import os
+import sys
+from typing import List, Dict, Tuple
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = set(['mp4'])
+
+DB_CONFIG = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'db',
+    'port': '3306',
+    'database': 'logs'
+}
+
+TIME_STAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+DB_CONNECTION = mysql.connector.connect(**DB_CONFIG)
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def logs() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'logs'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def get_logs() -> List[Tuple]:
+    cursor = DB_CONNECTION.cursor()
     cursor.execute('SELECT * FROM video_meta')
-    results = [{time_stamp: [video_name, frame_height, frame_width, frames_per_second, number_of_frames]}
-        for (time_stamp, video_name, frame_height, frame_width, frames_per_second, number_of_frames) in cursor]
+    results = [(ts.strftime(TIME_STAMP_FORMAT), vn, h, w, fps, count) for (ts, vn, h, w, fps, count) in cursor]
     cursor.close()
-    connection.close()
 
     return results
 
 
-@app.route('/logs')
-def index() -> str:
-    return json.dumps({'logs': logs()})
+def process_video(path: str) -> str:
+    cap = cv2.VideoCapture(path)
+    if cap.isOpened():
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        return (height, width, fps, frame_count)  # named tuple would be better here.
+    else:
+        raise IOError("Unable to process the video {:s}. Make sure the video format is correct".format(path))
+
+
+
+@app.route('/logs',  methods=['GET'])
+def logs() -> str:
+    return json.dumps({'logs': get_logs()})
+
+
+@app.route('/video',  methods=['POST'])
+def video() -> str:
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        height, width, fps, frame_count = process_video(path)
+        time_stamp = datetime.now().strftime(TIME_STAMP_FORMAT)
+        os.remove(path) # clean up
+        sql = "INSERT INTO video_meta (time_stamp, video_name, height, width, frames_per_second, frame_count) VALUES (%s, %s, %s, %s, %s, %s)"
+        meta = (time_stamp, filename, height, width, fps, frame_count)
+        cursor = DB_CONNECTION.cursor()
+        cursor.execute(sql, meta)
+        DB_CONNECTION.commit()
+        return json.dumps(meta)
+    else:
+        return "Something went wrong"
+
 
 
 if __name__ == '__main__':
--- db/init.sql
+++ db/init.sql
@@ -2,16 +2,10 @@ CREATE DATABASE logs;
 use logs;
 
 CREATE TABLE video_meta (
-  time_stamp VARCHAR(25),
+  time_stamp TIMESTAMP,
   video_name VARCHAR(255),
-  frame_height INT,
-  frame_width INT,
+  height INT,
+  width INT,
   frames_per_second INT,
-  number_of_frames INT
+  frame_count INT
 );
-
-INSERT INTO video_meta
-  (time_stamp, video_name, frame_height, frame_width, frames_per_second, number_of_frames)
-VALUES
-  ('2020-06-14 17:55:44', 'some_video.mp4', 480, 640, 60, 1800),
-  ('2020-06-14 17:59:12', 'some_other_video.mp4', 720, 1280, 30, 150);
